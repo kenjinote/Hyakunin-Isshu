fd1548106a4c8bdd0393d7e2b37be7077342c6c1
hack-jp <kenji256@gmail.com>
2015-05-29 11:32:47 +0900

    Add files

---
+++ main.cpp
@@ -0,0 +1,232 @@
+#pragma comment(lib,"imm32")
+#pragma comment(lib,"comctl32")
+
+#pragma comment(linker,"/opt:nowin98")
+#include<windows.h>
+#include<commctrl.h>
+#include<imm.h>
+#include"resource.h"
+
+CHAR szClassName[]="window";
+
+HWND hEdit,hList,hCeck;
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
+{
+	static HFONT hFont;
+	switch (msg)
+	{
+	case WM_COMMAND:
+		if(HIWORD(wParam) == EN_CHANGE || LOWORD(wParam) == 101)
+		{
+			TCHAR szEdit[256];
+			ListView_DeleteAllItems(hList);
+			if(GetWindowTextLength(hEdit)==0)break;
+			GetWindowText(hEdit,szEdit,256);
+			int i;
+			for(i=1;i<=100;i++)
+			{
+				CHAR szText[1024];
+				LoadString(
+					GetModuleHandle(NULL),
+					i,
+					szText,
+					sizeof(szText)/sizeof(szText[0])
+					);
+				BOOL bCeck=(
+					BST_CHECKED==SendMessage(
+					hCeck,BM_GETCHECK,0,0)
+					);
+				if(bCeck?
+					((BOOL)strstr(szText,szEdit))
+					:
+				(CSTR_EQUAL == CompareString(
+					LOCALE_SYSTEM_DEFAULT,
+					NORM_IGNOREKANATYPE,
+					szText,
+					strlen(szEdit),
+					szEdit,
+					strlen(szEdit))
+					))
+				{
+					char *token; 
+					token = strtok(szText, "\n"); 
+					token = strtok(NULL, "\n"); 
+					LV_ITEM		item;
+					item.mask = LVIF_TEXT;
+					item.pszText=token;
+					item.iItem = 0;
+					item.iSubItem = 0;
+					ListView_InsertItem(hList, &item);
+					token = strtok(NULL, "\n");
+					item.pszText=token;
+					item.iItem = 0;
+					item.iSubItem = 1;
+					ListView_SetItem(hList, &item);
+					token = strtok(NULL, "\n"); 
+					item.pszText=token;
+					item.iItem = 0;
+					item.iSubItem = 2;
+					ListView_SetItem(hList, &item);
+				}
+			}
+		}
+		break;
+	case WM_SETFOCUS:
+		SetFocus(hEdit);
+		SendMessage(hEdit,EM_SETSEL,0,-1);
+		break;
+	case WM_SIZE:
+		MoveWindow(hEdit,5,5,LOWORD(lParam)-10-64,28,TRUE);
+		MoveWindow(hCeck,LOWORD(lParam)-10-64+10,5,64,28,TRUE);
+		MoveWindow(hList,5,38,LOWORD(lParam)-10,HIWORD(lParam)-43,TRUE);
+		break;
+	case WM_CREATE:
+		InitCommonControls();
+		hEdit=CreateWindowEx(
+			WS_EX_CLIENTEDGE,
+			"EDIT",
+			0,
+			WS_CHILD|WS_VISIBLE|WS_TABSTOP,
+			0,
+			0,
+			0,
+			0,
+			hWnd,
+			(HMENU)100,
+			((LPCREATESTRUCT)(lParam))->hInstance,
+			NULL);
+		hCeck=CreateWindow(
+			"BUTTON",
+			"&Crazy",
+			WS_CHILD|WS_VISIBLE|BS_AUTOCHECKBOX|WS_TABSTOP,
+			0,
+			0,
+			0,
+			0,
+			hWnd,
+			(HMENU)101,
+			((LPCREATESTRUCT)(lParam))->hInstance,
+			NULL);
+		hList=CreateWindowEx(
+			WS_EX_CLIENTEDGE,
+			WC_LISTVIEW,
+			0,
+			WS_CHILD|WS_VISIBLE|LVS_REPORT|WS_TABSTOP,
+			0,
+			0,
+			0,
+			0,
+			hWnd,
+			(HMENU)102,
+			((LPCREATESTRUCT)(lParam))->hInstance,
+			NULL);
+		{//リストビューの選択を1行全体に
+			DWORD  dwStyle = ListView_GetExtendedListViewStyle(hList);
+			dwStyle |=  LVS_EX_FULLROWSELECT|LVS_EX_INFOTIP;
+			ListView_SetExtendedListViewStyle(hList, dwStyle); 
+		}
+		{//コントロールのフォントを明朝体に設定
+			hFont=CreateFont(
+				14,
+				0,
+				0,
+				0,
+				FW_NORMAL,
+				FALSE,
+				FALSE,
+				0,
+				SHIFTJIS_CHARSET,
+				OUT_DEFAULT_PRECIS,
+				CLIP_DEFAULT_PRECIS,
+				DEFAULT_QUALITY,
+				DEFAULT_PITCH,
+				"ＭＳ Ｐ明朝");
+			SendMessage(hEdit,WM_SETFONT,(WPARAM)hFont,0);
+			SendMessage(hList,WM_SETFONT,(WPARAM)hFont,0);
+		}
+		{//リストビューのヘッダー
+			LV_COLUMN	lvcol;
+			lvcol.mask = LVCF_FMT | LVCF_WIDTH | LVCF_TEXT | LVCF_SUBITEM;
+			lvcol.fmt = LVCFMT_LEFT;
+			lvcol.cx = 256;
+			lvcol.pszText = "上の句";
+			lvcol.iSubItem = 0;
+			ListView_InsertColumn(hList, 0, &lvcol);
+			
+			lvcol.fmt = LVCFMT_LEFT;
+			lvcol.cx = 256;
+			lvcol.pszText = "下の句";
+			lvcol.iSubItem = 1;
+			ListView_InsertColumn(hList, 1, &lvcol);
+			
+			lvcol.fmt = LVCFMT_LEFT;
+			lvcol.cx = 128;
+			lvcol.pszText = "歌人";
+			lvcol.iSubItem = 2;
+			ListView_InsertColumn(hList, 2, &lvcol);
+		}
+		{//IMEをON
+			HIMC hIMC = ImmGetContext( hWnd );
+			ImmSetOpenStatus( hIMC, TRUE );
+			ImmReleaseContext( hWnd, hIMC );
+		}
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hWnd);
+		break;
+	case WM_DESTROY:
+		DeleteObject(hFont);
+		PostQuitMessage(0);
+		break;
+	default:
+		return(DefDlgProc(hWnd,msg,wParam,lParam));
+}
+return (0L);
+}
+
+
+EXTERN_C void __cdecl WinMainCRTStartup()
+{
+	MSG msg;
+	HINSTANCE hInstance=GetModuleHandle(0);
+	WNDCLASS wndclass={
+		0,
+			WndProc,
+			0,
+			DLGWINDOWEXTRA,
+			hInstance,
+			0,
+			LoadCursor(0,IDC_ARROW),
+			0,
+			0,
+			szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd=CreateWindow(szClassName,
+		"百人一首検索くん",
+		WS_OVERLAPPEDWINDOW|WS_CLIPCHILDREN,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+	ShowWindow(hWnd,SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while(GetMessage(&msg,0,0,0))
+	{
+		if(!IsDialogMessage(hWnd,&msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	ExitProcess(msg.wParam);
+}
+
+#if _DEBUG
+void main(){}
+#endif
---
+++ resource.h
@@ -0,0 +1,115 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by res.rc
+//
+#define IDS_1                           1
+#define IDS_2                           2
+#define IDS_3                           3
+#define IDS_4                           4
+#define IDS_5                           5
+#define IDS_6                           6
+#define IDS_7                           7
+#define IDS_8                           8
+#define IDS_9                           9
+#define IDS_10                          10
+#define IDS_11                          11
+#define IDS_12                          12
+#define IDS_13                          13
+#define IDS_14                          14
+#define IDS_15                          15
+#define IDS_16                          16
+#define IDS_17                          17
+#define IDS_18                          18
+#define IDS_19                          19
+#define IDS_20                          20
+#define IDS_21                          21
+#define IDS_22                          22
+#define IDS_23                          23
+#define IDS_24                          24
+#define IDS_25                          25
+#define IDS_26                          26
+#define IDS_27                          27
+#define IDS_28                          28
+#define IDS_29                          29
+#define IDS_30                          30
+#define IDS_31                          31
+#define IDS_32                          32
+#define IDS_33                          33
+#define IDS_34                          34
+#define IDS_35                          35
+#define IDS_36                          36
+#define IDS_37                          37
+#define IDS_38                          38
+#define IDS_39                          39
+#define IDS_40                          40
+#define IDS_41                          41
+#define IDS_42                          42
+#define IDS_43                          43
+#define IDS_44                          44
+#define IDS_45                          45
+#define IDS_46                          46
+#define IDS_47                          47
+#define IDS_48                          48
+#define IDS_49                          49
+#define IDS_50                          50
+#define IDS_51                          51
+#define IDS_52                          52
+#define IDS_53                          53
+#define IDS_54                          54
+#define IDS_55                          55
+#define IDS_56                          56
+#define IDS_57                          57
+#define IDS_58                          58
+#define IDS_59                          59
+#define IDS_60                          60
+#define IDS_61                          61
+#define IDS_62                          62
+#define IDS_63                          63
+#define IDS_64                          64
+#define IDS_65                          65
+#define IDS_66                          66
+#define IDS_67                          67
+#define IDS_68                          68
+#define IDS_69                          69
+#define IDS_70                          70
+#define IDS_71                          71
+#define IDS_72                          72
+#define IDS_73                          73
+#define IDS_74                          74
+#define IDS_75                          75
+#define IDS_76                          76
+#define IDS_77                          77
+#define IDS_78                          78
+#define IDS_79                          79
+#define IDS_80                          80
+#define IDS_81                          81
+#define IDS_82                          82
+#define IDS_83                          83
+#define IDS_84                          84
+#define IDS_85                          85
+#define IDS_86                          86
+#define IDS_87                          87
+#define IDS_88                          88
+#define IDS_89                          89
+#define IDS_90                          90
+#define IDS_91                          91
+#define IDS_92                          92
+#define IDS_93                          93
+#define IDS_94                          94
+#define IDS_95                          95
+#define IDS_96                          96
+#define IDS_97                          97
+#define IDS_98                          98
+#define IDS_99                          99
+#define IDS_100                         100
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
